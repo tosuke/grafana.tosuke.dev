cbd4485a367f64112e261b78636b863105e5feb3
tosuke <13393900+tosuke@users.noreply.github.com>
2026-08-18 16:03:16 +0900

    refactor: optimize bundles

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 import { grafana } from "./grafana";
 import { createLitestreamApp } from "./lib/litestream";
 
@@ -10,4 +11,14 @@ app.route("/_litestream", litestreamApp);
 
 app.all("*", async (c) => {
   return grafana().fetch(c.req.raw);
+});
+
+app.onError(async (err) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+
+  // To support source maps in stack traces, we re-throw the error so that Tail Workers can handle it and provide a proper stack trace.
+  // FYI: https://developers.cloudflare.com/workers/observability/source-maps/
+  throw err;
 });
